53ec13fce6b058c37426ff6451e9cf4b8b7e4293
Sergey Panin <psdarko@gmail.com>
2018-04-20 11:54:04 +0300

    Add textarea field

--- src/common/components/Form/fields.js
+++ src/common/components/Form/fields.js
@@ -2,12 +2,16 @@ import semanticFieldWrapper from './semantic-field-wrapper';
 
 import Select from './SelectFix/SelectFix';
 import InputWithState from './InputWithState/InputWithState';
+import TextAreaWithState from './TextAreaWithState/TextAreaWithState';
 import DatePicker from './DatePicker/DatePicker';
 
 export const SelectField = semanticFieldWrapper(Select, {
   supportOutLabel: true,
 });
 export const InputField = semanticFieldWrapper(InputWithState, {
+  supportOutLabel: true,
+});
+export const TextAreaField = semanticFieldWrapper(TextAreaWithState, {
   supportOutLabel: true,
 });
 export const DateRangeField = semanticFieldWrapper(DatePicker, {
