1b892be6f0fc3eaf30f3e210fdf5ee11103407e6
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-08-28 10:40:17 +0300

    chore(build): - можно разместить кору не только в npm но и в отдельной папке, поэтому билд скрипты могут быть запущены не только в node_modules

--- build-scripts/webpack-context.js
+++ build-scripts/webpack-context.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 const PARENT_WEBPACK_CONTEXT = require('@reagentum/front-core/build-scripts/webpack-context');
 
+const PROCESS_PATH = process.cwd();
 const CURRENT_FILE_PATH = __dirname;
 
-const useFromFrontCore = CURRENT_FILE_PATH.indexOf('node_modules') < 0;
+// const useFromFrontCore = CURRENT_FILE_PATH.indexOf('node_modules') < 0;
+const useFromFrontCore = CURRENT_FILE_PATH.indexOf(PROCESS_PATH) >= 0;
 
 function inFrontCoreComponentsProject(...args) {
   return path.resolve(CURRENT_FILE_PATH, '..', ...args);
