77ac6c079ae176a6498aa8fb5c0efa41ac72596c
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-08-28 18:17:49 +0300

    chore(build): - фикс чтобы можно было кору разворачивать не только из node_modules

--- build-scripts/webpack-context.js
+++ build-scripts/webpack-context.js
@@ -1,18 +1,20 @@
 const path = require('path');
 
+const { pathJoin } = require('@reagentum/front-core/build-scripts/utils/path-utils');
 const PARENT_WEBPACK_CONTEXT = require('@reagentum/front-core/build-scripts/webpack-context');
 
 const PROCESS_PATH = process.cwd();
 const CURRENT_FILE_PATH = __dirname;
 
 // const useFromFrontCore = CURRENT_FILE_PATH.indexOf('node_modules') < 0;
-const useFromFrontCore = CURRENT_FILE_PATH.indexOf(PROCESS_PATH) >= 0;
+const useFromFrontCoreComponents = CURRENT_FILE_PATH.indexOf(pathJoin(PROCESS_PATH, 'src')) >= 0;
+
 
 function inFrontCoreComponentsProject(...args) {
   return path.resolve(CURRENT_FILE_PATH, '..', ...args);
 }
 
-const appStyleConfig = require(useFromFrontCore
+const appStyleConfig = require(useFromFrontCoreComponents
   ? inFrontCoreComponentsProject('src/common/app-styles/vars.js')
   : inFrontCoreComponentsProject('lib/common/app-styles/vars.js')
 );
