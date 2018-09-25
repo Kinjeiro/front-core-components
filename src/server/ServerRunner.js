fb00be7031071e4bf4b0752e948bf2f7f88402dc
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-09-25 17:22:11 +0300

    bug(modules): - забыл подключить модули серверные

--- src/server/ServerRunner.js
+++ src/server/ServerRunner.js
@@ -1,13 +1,24 @@
-import bind from 'lodash-decorators/bind';
+// import bind from 'lodash-decorators/bind';
 
+import SubModuleFactory from '@reagentum/front-core/lib/modules/SubModuleFactory';
 import ParentServerRunner from '@reagentum/front-core/lib/server/CoreServerRunner';
-import serverConfig from '@reagentum/front-core/lib/server/server-config';
+// import serverConfig from '@reagentum/front-core/lib/server/server-config';
 
 import ClientRunner from '../client/ClientRunner';
 
 export default class ServerRunner extends ParentServerRunner {
   getClientRunner() {
     return new ClientRunner();
+  }
+
+  loadServerSubModules() {
+    return [
+      ...super.loadServerSubModules(),
+      ...SubModuleFactory.loadSubModules(
+        require.context('../modules', true, /^\.\/(.*)\/server\/index\.js/gi),
+        SubModuleFactory.SUB_MODULE_TYPES.SERVER
+      ),
+    ];
   }
 }
 
