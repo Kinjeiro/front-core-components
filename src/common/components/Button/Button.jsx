6751fc74a5888d085ba6d24824faf5450f81cb40
Andrey Kuzmin <kinjeiro@gmail.com>
2018-12-08 22:18:11 +0300

    feat(CB, Button): - добавил флаг asyncIsLoading - если действие асинхронное то появится лоадинг на кнопке (больше не нужно actionStatus протаскивать) \\ также в loading можно подавать reduxActionStatus

--- src/common/components/Button/Button.jsx
+++ src/common/components/Button/Button.jsx
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import bind from 'lodash-decorators/bind';
 
+import STATUS_PROP_TYPE from '@reagentum/front-core/lib/common/models/action-status';
+import { emitProcessing } from '@reagentum/front-core/lib/common/utils/common';
+
 import { Button as ButtonSemantic } from 'semantic-ui-react';
 
 import './Button.scss';
@@ -9,12 +12,44 @@ import './Button.scss';
 export default class Button extends PureComponent {
   static propTypes = {
     ...ButtonSemantic.propTypes,
+    loading: PropTypes.oneOfType([
+      PropTypes.bool,
+      STATUS_PROP_TYPE,
+    ]),
     simple: PropTypes.bool,
+    asyncIsLoading: PropTypes.bool,
   };
 
   static defaultProps = {
     type: 'button',
+    asyncIsLoading: false,
   };
+
+  state = {
+    isProcessing: undefined,
+  };
+
+  // ======================================================
+  // HANDLERS
+  // ======================================================
+  @bind()
+  handleClick(...args) {
+    const {
+      onClick,
+      asyncIsLoading,
+    } = this.props;
+
+    if (onClick) {
+      if (asyncIsLoading) {
+        return emitProcessing(
+          onClick(...args),
+          this,
+        );
+      }
+      return onClick(...args);
+    }
+    return true;
+  }
 
   // ======================================================
   // MAIN RENDER
@@ -22,13 +57,34 @@ export default class Button extends PureComponent {
   render() {
     const {
       simple,
+      loading,
+      asyncIsLoading,
       ...semanticProps
     } = this.props;
+    const {
+      isProcessing,
+    } = this.state;
+
+
+    const isLoading = typeof loading !== 'undefined'
+      ? typeof loading === 'object' && loading !== null && typeof loading.isFetching !== 'undefined'
+        ? loading.isFetching
+        : loading
+      : asyncIsLoading
+        ? isProcessing
+        : undefined;
 
     return (
       <ButtonSemantic
         { ...semanticProps }
-        className={ `Button ${simple ? 'Button--simple' : ''} ${semanticProps.className || ''}` }
+        onClick={ this.handleClick }
+        loading={ isLoading }
+        className={ `\
+          Button\
+          ${simple ? 'Button--simple' : ''}\
+          ${isLoading ? 'Button--loading' : ''}\
+          ${semanticProps.className || ''}\
+        ` }
       />
     );
   }
